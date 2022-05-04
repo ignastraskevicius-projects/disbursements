117d76dc66ee8bc6f4734bd58ae455c9a6e7e2f8
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-05-04 20:54:56 +0300

    domain delegates disbursement calculations to persistence

--- domain/src/main/java/org/ignast/challenge/ecommerce/disbursements/domain/DisbursementsRepository.java
+++ domain/src/main/java/org/ignast/challenge/ecommerce/disbursements/domain/DisbursementsRepository.java
@@ -5,4 +5,12 @@ import java.util.List;
 
 public interface DisbursementsRepository {
     public List<DisbursementOverWeekPeriod> findByLastDayOfWeekPeriod(final LocalDate date);
+
+    public List<DisbursementOverWeekPeriod> calculateDisbursementsForWeekEndingWith(
+        final LocalDate dateLastOfWeek
+    );
+
+    public Iterable<DisbursementOverWeekPeriod> saveAll(
+        final Iterable<DisbursementOverWeekPeriod> disbursements
+    );
 }
--- domain/src/test/java/org/ignast/challenge/ecommerce/disbursements/domain/DisbursementsTest.java
+++ domain/src/test/java/org/ignast/challenge/ecommerce/disbursements/domain/DisbursementsTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
@@ -36,6 +37,19 @@ class DisbursementsTest {
 
         assertThatExceptionOfType(NoDisbursementsFound.class)
             .isThrownBy(() -> this.disbursements.retrieveDisbursementsOverWeekEndingBefore(anyDate()));
+    }
+
+    @Test
+    public void shouldCalculateDisbursementsForWeekBeforeGivenDate() {
+        val dateAfterLastDay = LocalDate.of(2022, 1, 8);
+        val dateLastOfWeek = LocalDate.of(2022, 1, 7);
+        val expectedDisbursements = List.of(mock(DisbursementOverWeekPeriod.class));
+        when(repository.calculateDisbursementsForWeekEndingWith(dateLastOfWeek))
+            .thenReturn(expectedDisbursements);
+
+        disbursements.calculateDisbursementsForWeekEndingBefore(dateAfterLastDay);
+
+        verify(repository).saveAll(expectedDisbursements);
     }
 
     private LocalDate anyDate() {
