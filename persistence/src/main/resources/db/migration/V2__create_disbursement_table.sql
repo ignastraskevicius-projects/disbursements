e5e93001a94d784431379ac6cbf0bdfe974a2518
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-05-04 11:03:46 +0300

    db field renames

--- persistence/src/main/resources/db/migration/V2__create_disbursement_table.sql
+++ persistence/src/main/resources/db/migration/V2__create_disbursement_table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE disbursements_over_week (
+CREATE TABLE disbursement_over_week_period (
     id integer AUTO_INCREMENT PRIMARY KEY,
     merchant_id VARCHAR(256),
-    last_day_of_week DATE,
+    last_day_of_week_period DATE,
     disbursement_amount DECIMAL(20,7),
-    CONSTRAINT unique_date_merchant UNIQUE(merchant_id, last_day_of_week),
-    INDEX (last_day_of_week)
+    CONSTRAINT unique_date_merchant UNIQUE(merchant_id, last_day_of_week_period)
 );
+CREATE INDEX disbursement_days ON disbursement_over_week_period(last_day_of_week_period);
